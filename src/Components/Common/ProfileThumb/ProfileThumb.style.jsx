0054688d787a8c37a042a5a21fbc5839965c5b03
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-13 16:18:10 +0900

    Feat: 프로필 썸네일 컴포넌트 구현 #4

---
+++ src/Components/Common/ProfileThumb/ProfileThumb.style.jsx
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+import basicProfileImage from '../../../Assets/Images/img_profile_basic.png';
+
+const setSize = size => {
+  switch (size) {
+    case 'small':
+      return css`
+        width: 36px;
+        height: 36px;
+      `;
+
+    default:
+      return css`
+        width: 110px;
+        height: 110px;
+      `;
+  }
+};
+
+export const ProfileThumbWrapper = styled.div`
+  ${({ size }) => setSize(size)}
+
+  position: relative;
+  border-radius: 50%;
+  box-sizing: border-box;
+  background: url(${basicProfileImage}) no-repeat center / calc(100% + 2px) auto;
+  overflow: hidden;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
