8514933801c733d0f901fa1b545a6e25fc7c38e8
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-13 17:38:05 +0900

    Feat: 라우터 경로에 따른 색상 변경 기능 추가

--- src/Components/Common/TabMenu/TabMenu.style.jsx
+++ src/Components/Common/TabMenu/TabMenu.style.jsx
@@ -1,8 +1,49 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import iconHome from '../../../Assets/Icons/icon_home.png';
+import iconHomeFill from '../../../Assets/Icons/icon_home_fill.png';
 import iconChat from '../../../Assets/Icons/icon_chat.png';
+import iconChatFill from '../../../Assets/Icons/icon_chat_fill.png';
 import iconPost from '../../../Assets/Icons/icon_post.png';
 import iconProfile from '../../../Assets/Icons/icon_profile.png';
+import iconProfileFill from '../../../Assets/Icons/icon_profile_fill.png';
+
+const setActiveIcon = type => {
+  switch (type) {
+    case '/chat':
+      return css`
+        &.chat {
+          color: ${({ theme }) => theme.colors.primary};
+
+          &::before {
+            background-image: url(${iconChatFill});
+          }
+        }
+      `;
+    case '/profile':
+      return css`
+        &.profile {
+          color: ${({ theme }) => theme.colors.primary};
+
+          &::before {
+            background-image: url(${iconProfileFill});
+          }
+        }
+      `;
+
+    default:
+      return css`
+        &.home {
+          color: ${({ theme }) => theme.colors.primary};
+
+          color: ${({ theme }) => theme.colors.primary};
+
+          &::before {
+            background-image: url(${iconHomeFill});
+          }
+        }
+      `;
+  }
+};
 
 export const TabMenuWrapper = styled.nav`
   display: flex;
@@ -44,5 +85,7 @@ export const TabMenuWrapper = styled.nav`
     &.profile::before {
       background-image: url(${iconProfile});
     }
+
+    ${({ type }) => setActiveIcon(type)}
   }
 `;
