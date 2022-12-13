18b881c94b46a1f7f14b145a7fd584daad9d3cc2
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-13 17:31:18 +0900

    Feat: 탭 메뉴 컴포넌트 마크업

--- src/Components/Common/TabMenu/TabMenu.jsx
+++ src/Components/Common/TabMenu/TabMenu.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { TabMenuWrapper } from './TabMenu.style';
 
 export default function TabMenu() {
-  return <div>TabMenu</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <TabMenuWrapper type={pathname}>
+      <a className='home' href='/'>
+        홈
+      </a>
+      <a className='chat' href='/'>
+        채팅
+      </a>
+      <a className='post' href='/'>
+        게시물 작성
+      </a>
+      <a className='profile' href='/'>
+        프로필
+      </a>
+    </TabMenuWrapper>
+  );
 }
