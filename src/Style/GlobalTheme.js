d036bb262742650ba2e84c7259ac68af60ea5109
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-14 16:39:38 +0900

    ETC: ir 클래스 및 placeholder 테마 컬러 추가

--- src/Style/GlobalTheme.js
+++ src/Style/GlobalTheme.js
@@ -3,7 +3,7 @@ const fontSizes = {
   md: '1.8rem',
   base: '1.4rem',
   sm: '1.2rem',
-  xs: '1rem',
+  sx: '1rem',
 };
 
 const colors = {
@@ -15,6 +15,7 @@ const colors = {
   background: '#dbdbdb',
   mainText: '#000',
   subText: '#767676',
+  placeholder: '#c4c4c4c',
 };
 
 const GlobalTheme = {
