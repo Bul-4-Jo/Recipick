58bdf2076d12806a1a80e255fdf17d34ca059cdf
강수정 <kngsujng@naver.com>
2022-12-15 09:46:31 +0900

    Feat: SearchHeader, UploadHeader 기능구현

--- src/Components/Common/Header/UploadHeader.jsx
+++ src/Components/Common/Header/UploadHeader.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import iconBack from '../../../Assets/Icons/icon_back.png';
 
-export default function Pre() {
+const HeaderWrapper = styled.header`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+  z-index: 10;
+  background-color: #fff;
+`;
+const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  padding: 0 16px;
+
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export default function UploadHeader() {
+  const navigate = useNavigate();
+
   return (
-    <div>Pre</div>
-  )
+    <>
+      <HeaderWrapper>
+        <Div>
+          <button
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            <img src={iconBack} alt='뒤로가기 버튼'></img>
+          </button>
+        </Div>
+      </HeaderWrapper>
+    </>
+  );
 }
