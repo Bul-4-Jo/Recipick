5ba380c1c243fb22b59d0c5fddc731cc66927a63
yunkooo <yunkoooooo@gmail.com>
2022-12-15 14:37:45 +0900

    Design: LoginForm 컴포턴트 구현- #29

---
+++ src/Components/LoginForm/LoginForm.style.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const LoginFormWrapper = styled.div`
+  width: 322px;
+  margin: 0 auto;
+`;
+
+export const InpWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 45px;
+  gap: 27px;
+`;
+
+export const InpLabel = styled.label`
+  margin-bottom: 5px;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`;
+
+export const Inp = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LoginInp = styled.input`
+  border-radius: 44px;
+  outline: 1px solid ${({ theme }) => theme.colors.border};
+  border: none;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  padding: 14px;
+
+  &:focus {
+    outline: 1px solid ${({ theme }) => theme.colors.secondary};
+    border: none;
+  }
+`;
