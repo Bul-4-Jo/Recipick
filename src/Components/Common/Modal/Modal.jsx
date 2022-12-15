b04e00e9cdfca6a23b6d5f001e1006348bdc1258
yunkooo <yunkoooooo@gmail.com>
2022-12-15 10:44:13 +0900

    Design: modal 상단 바 추가 - #17

--- src/Components/Common/Modal/Modal.jsx
+++ src/Components/Common/Modal/Modal.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { ModalWrapper, ButtonItem, BackgroundBlur } from './Modal.style';
 
-export default function Modal({ listObj }) {
+export default function Modal() {
   // list Obj 아래와 같은 형식으로 전달 받아 활용
-  // const listObj = [
-  //   {
-  //     name: '삭제',
-  //     func: () => console.log('삭제'),
-  //   },
-  //   {
-  //     name: '수정',
-  //     func: () => console.log('수정'),
-  //   },
-  //   {
-  //     name: '웹사이트에서 상품 보기',
-  //     func: () => console.log('상품 보기'),
-  //   },
-  // ];
+  const listObj = [
+    {
+      name: '삭제',
+      func: () => console.log('삭제'),
+    },
+    {
+      name: '수정',
+      func: () => console.log('수정'),
+    },
+    {
+      name: '웹사이트에서 상품 보기',
+      func: () => console.log('상품 보기'),
+    },
+  ];
 
   return (
     <>
