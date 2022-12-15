cdda1a2adb66a3225ab408be55e8ec265029ab4c
강수정 <kngsujng@naver.com>
2022-12-15 18:52:30 +0900

    Fix: 프로필수정 페이지 오류 수정

--- src/Pages/Main/Profile/ProfileEdit/ProfileImg/ProfileImg.jsx
+++ src/Pages/Main/Profile/ProfileEdit/ProfileImg/ProfileImg.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { profileImgWrapper } from './ProfileImg.style';
+import { ProfileImgWrapper } from './ProfileImg.style';
 import profileUplaodBtn from '../../../../../Assets/Icons/icon_uploadBtn.png';
 import profileImg from '../../../../../Assets/Images/img_profile_basic.png';
 
 export default function ProfileImg() {
   return (
     <>
-      <profileImgWrapper>
+      <ProfileImgWrapper>
         <img src={profileImg} alt='기본 프로필 이미지'></img>
         <button>
           <img src={profileUplaodBtn} alt='프로필 이미지 업로드 버튼'></img>
         </button>
-      </profileImgWrapper>
+      </ProfileImgWrapper>
     </>
   );
 }
