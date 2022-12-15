4ba74bb70444fed017b2134f05b0c755b1cb6ed0
isemae <foxlis97@naver.com>
2022-12-15 16:20:56 +0900

    Design: 유저 검색 리스트 마크업 및 스타일링

---
+++ src/Pages/Main/Search/SearchBar.style.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+// import HeaderContainer from '../../../Components/Common/Header/Header.style';
+
+export const SearchInputBar = styled.input`
+  background-color: #f2f2f2;
+  width: 100%;
+  padding: 7px 16px;
+  margin-left: 20px;
+  border: none;
+  border-radius: 32px;
+  :focus {
+    outline: none;
+  }
+`;
+
+function SearchBar() {
+  return (
+    <>
+      <SearchInputBar type='text' placeholder='계정 검색' />
+    </>
+  );
+}
+
+export default SearchBar;
