644cef453ab0bfd5b1f07b92ad8474e494ff7a66
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-15 09:26:36 +0900

    Design: 상품 이미지 업로드 컴포넌트 스타일링 #21

---
+++ src/Components/ProductEdit/ProductImg/ProductImg.style.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const ProductImgWrapper = styled.div``;
+
+export const Title = styled.strong`
+  color: ${({ theme }) => theme.colors.subText};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`;
+
+export const ImgWrapper = styled.div`
+  position: relative;
+  margin-top: 18px;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  border-radius: 10px;
+  overflow: hidden;
+  aspect-ratio: 161 / 118;
+  background-color: ${({ theme }) => theme.colors.background};
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  label {
+    position: absolute;
+    bottom: 12px;
+    right: 12px;
+    width: 36px;
+    height: 36px;
+    cursor: pointer;
+  }
+
+  input {
+    display: none;
+  }
+`;
