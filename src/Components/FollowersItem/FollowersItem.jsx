7555ae84b19201462ceb6a7436343ffffeee51c5
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-17 01:32:52 +0900

    Feat: 팔로워 리스트 요청 및 팔로우, 언팔로우 기능 구현

--- src/Components/FollowersItem/FollowersItem.jsx
+++ src/Components/FollowersItem/FollowersItem.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { FollowersItemWrapper, ContentWrapper, Text, Title } from './FollowersItem.style';
 import ProfileThumb from '../Common/ProfileThumb/ProfileThumb';
 import Button from '../Common/Button/Button.style';
 
-export default function FollowersItem() {
+export default function FollowersItem({ follower }) {
+  const { accountname, username, isfollow, image, intro } = follower;
+  const [isFollowing, setIsFollowing] = useState(isfollow);
+
+  const baseURL = process.env.REACT_APP_URL;
+  const token = process.env.REACT_APP_TOKEN;
+
+  const instance = axios.create({
+    baseURL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-type': 'application/json',
+    },
+  });
+
+  const onClickHandler = () => {
+    isFollowing ? unFollow() : follow();
+  };
+
+  const follow = async () => {
+    try {
+      const response = await instance.post(`/profile/${accountname}/follow`);
+
+      setIsFollowing(response.data.profile.isfollow);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const unFollow = async () => {
+    try {
+      await instance.delete(`/profile/${accountname}/unfollow`);
+      setIsFollowing(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <FollowersItemWrapper>
-      <ProfileThumb size='large' />
+      <ProfileThumb size='large' src={image} />
       <ContentWrapper>
-        <Title>애월읍 한라봉 최고 맛집</Title>
-        <Text>
-          정성을 다해 농사짓는 한라봉정성을 다해 농사짓는 한라봉정성을 다해 농사짓는 한라봉정성을 다해 농사짓는
-          한라봉정성을 다해 농사짓는 한라봉
-        </Text>
+        <Title>{username}</Title>
+        <Text>{intro}</Text>
       </ContentWrapper>
-      <Button className='small' content='팔로우' />
+      {isFollowing ? (
+        <Button className='small' content='취소' disabled={false} active onClick={onClickHandler} />
+      ) : (
+        <Button className='small' content='팔로우' disabled={false} onClick={onClickHandler} />
+      )}
     </FollowersItemWrapper>
   );
 }
