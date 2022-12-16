127a7640eb36bc72b5f42198a75009e204a36b0d
yunkooo <yunkoooooo@gmail.com>
2022-12-16 18:30:45 +0900

    Design: signup 페이지 마크업 및 스타일링 - #39

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Button from '../../Components/Common/Button/Button.style';
+import { SignupWrapper, SignupTitle, Input, InputWrapper, Label } from './Signup.style';
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <SignupWrapper>
+      <SignupTitle>이메일로 회원가입</SignupTitle>
+      <form>
+        <InputWrapper>
+          <Label htmlFor='inpEmail'>이메일</Label>
+          <Input id='inpEmail' type='email' placeholder='이메일 주소를 입력해주세요.' required />
+        </InputWrapper>
+
+        <InputWrapper>
+          <label htmlFor='inpPassword'>비밀번호</label>
+          <Input id='inpPassword' type='password' placeholder='비밀번호를 설정해 주세요.' required />
+        </InputWrapper>
+        <Button className='large' content='다음' />
+      </form>
+    </SignupWrapper>
+  );
 }
 
 export default Signup;
---
+++ src/Pages/Signup/Signup.style.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const SignupWrapper = styled.section`
+  max-width: 780px;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SignupTitle = styled.h1`
+  text-align: center;
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  margin: 30px 0;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 10px;
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+`;
+
+export const Input = styled.input`
+  width: 322px;
+  border: none;
+  padding: 10px 5px;
+  font-size: ${({ theme }) => theme.fontSizes.base};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+
+  &:focus {
+    outline: none;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
+`;
