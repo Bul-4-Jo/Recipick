94dde017e30384ace451d271ccaf00ac0f75bdec
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-19 15:03:34 +0900

    Design: 홈피드 페이지 마크업 및 스타일링 #53

--- src/Pages/Main/HomeFeed/HomeFeed.jsx
+++ src/Pages/Main/HomeFeed/HomeFeed.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HomeFeedWrapper, TextDesc } from './HomeFeed.style';
+import Button from './../../../Components/Common/Button/Button.style';
+import LogoCharacter from '../../../Assets/Images/logo_character.svg';
 
-function HomeFeed() {
-  return <div>HomeFeed</div>;
+export default function HomeFeed() {
+  const [feedList, setFeedList] = useState([]);
+
+  return (
+    <HomeFeedWrapper>
+      {feedList.length ? (
+        <p>feedList</p>
+      ) : (
+        <>
+          <img src={LogoCharacter} alt='' />
+          <TextDesc>유저를 검색해 팔로우 해보세요!</TextDesc>
+          <Button className='mediumSmall' content='검색하기' disabled={false} onClick={onClickHandler} />
+        </>
+      )}
+    </HomeFeedWrapper>
+  );
 }
-
-export default HomeFeed;
