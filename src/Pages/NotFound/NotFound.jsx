8139545772f646c1ef27fb3e17d510f1db825ec2
강수정 <kngsujng@naver.com>
2022-12-20 02:25:35 +0900

    Etc: NonFound 페이지 원상 복구

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-// import BasicHeader from '../../Components/Common/Header/BasicHeader';
-import ChatHeader from '../../Components/Common/Header/ChatHeader';
-import MainHeader from '../../Components/Common/Header/MainHeader';
-import SearchHeader from '../../Components/Common/Header/SearchHeader';
-import UploadHeader from '../../Components/Common/Header/UploadHeader';
 
-function NotFound() {
-  return <MainHeader />;
+export default function NotFound() {
+  return <div>NotFound</div>;
 }
-
-export default NotFound;
