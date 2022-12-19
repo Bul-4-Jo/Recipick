4b10d5d849914cfbaa6190e8496609177fa628bc
yunkooo <yunkoooooo@gmail.com>
2022-12-19 15:38:29 +0900

    Design: NotFound 페이지 컴포넌트 마크업 및 스타일링

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../Assets/Images/NotFound_img.svg';
+import Button from '../../Components/Common/Button/Button.style';
+import { SectionWrapper, ContentWrapper } from './NotFound.style';
 
 function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <SectionWrapper>
+      <ContentWrapper>
+        <Logo width='300px' height='300px' />
+        <Button onClick={() => navigate(-1)} className='medium' active={false} disabled={false} content={'Go Back'} />
+      </ContentWrapper>
+    </SectionWrapper>
+  );
 }
 
 export default NotFound;
