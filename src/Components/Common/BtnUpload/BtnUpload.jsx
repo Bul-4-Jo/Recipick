e10eea11e96f74e35e6e03caf7098f9f11dfd013
sduu <qjqmfrja00@gmail.com>
2022-12-21 14:19:59 +0900

    Refactor: 이미지 업로드 버튼 컴포넌트 분리

---
+++ src/Components/Common/BtnUpload/BtnUpload.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BtnUploadWrapper, ImgInp, ImgLabel } from './BtnUpload.style';
+import ImgBtnUpload from '../../../Assets/Images/btn_upload_file_mainColor.png';
+
+export default function BtnUpload() {
+  return (
+    <BtnUploadWrapper>
+      <ImgLabel htmlFor='userImgInp'>
+        <img src={ImgBtnUpload} alt='유저 이미지 등록' />
+      </ImgLabel>
+      <ImgInp id='userImgInp' type='file' />
+    </BtnUploadWrapper>
+  );
+}
--- src/Components/PostEdit/BtnImgUpload/BtnImgUpload.style.jsx
+++ src/Components/PostEdit/BtnImgUpload/BtnImgUpload.style.jsx
@@ -1,15 +1,27 @@
 import styled from 'styled-components';
 
 export const BtnImgUploadWrapper = styled.div`
-  position: absolute;
-  bottom: 16px;
-  right: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  max-width: 780px;
+  margin: 0 auto;
+  pointer-events: none;
+
+  div {
+    position: absolute;
+    bottom: 86px;
+    right: 16px;
+  }
 
   label {
     display: block;
     width: 50px;
     height: 50px;
     cursor: pointer;
+    pointer-events: visible;
 
     img {
       width: 100%;
