7bda915877f630efdf8603359937604ff96c7adf
sduu <qjqmfrja00@gmail.com>
2022-12-21 09:24:07 +0900

    Refactor: 헤더 컴포넌트 분리 및 레이아웃 추가

---
+++ src/Components/Common/Header/Header.style.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.border};
+  background-color: #fff;
+`;
+export const Div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 48px;
+  padding: 0 16px;
+
+  h2 {
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+    font-size: 700;
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    img {
+      width: 24px;
+    }
+  }
+`;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -32,7 +32,7 @@ import NotFound from '../Pages/NotFound/NotFound';
 import PostEdit from '../Pages/Post/PostEdit/PostEdit';
 import PostDetail from '../Pages/PostDetail';
 import Signup from '../Pages/Signup/Signup';
-import { LayoutMain, LayoutIntro } from '../Components/Common/Layout/Layout';
+import { LayoutMain, LayoutIntro, LayOutJoin } from '../Components/Common/Layout/Layout';
 import ProfileSet from '../Pages/Signup/Profile/ProfileSet';
 import ProductEdit from '../Pages/Product/ProductEdit/ProductEdit';
 
@@ -42,10 +42,11 @@ export default function Router() {
       <Routes>
         <Route element={<LayoutIntro />}>
           <Route path='/' element={<Splash />} />
-          <Route path='/join' element={<Signup />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/login/signup' element={<Signup />} />
+        </Route>
 
+        <Route element={<LayOutJoin />}>
+          <Route path='/login/signup' element={<Signup />} />
           <Route path='/login/singup/profileset' element={<ProfileSet />} />
         </Route>
 
