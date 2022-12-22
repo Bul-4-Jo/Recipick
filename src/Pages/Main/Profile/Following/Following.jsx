8e227399ac24083989b590a976fe9fa0426d8710
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-22 22:40:29 +0900

    Feat: 팔로잉 페이지 구현 #76 #77

--- src/Pages/Main/Profile/Following/Following.jsx
+++ src/Pages/Main/Profile/Following/Following.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FollowingWrapper } from './Following.style';
+import { getFollowingList } from '../../../../Hooks/useApi';
+import FollowItem from '../../../../Components/FollowItem/FollowItem';
 
-function Following() {
-  return <div>Following</div>;
+export default function Following() {
+  const [followingList, setFollowingList] = useState([]);
+
+  useEffect(() => {
+    getFollowingList().then(response => setFollowingList(response));
+  }, []);
+
+  return (
+    <>
+      <FollowingWrapper>
+        <ul>
+          {followingList && followingList.map((following, index) => <FollowItem followInfo={following} key={index} />)}
+        </ul>
+      </FollowingWrapper>
+    </>
+  );
 }
-
-export default Following;
