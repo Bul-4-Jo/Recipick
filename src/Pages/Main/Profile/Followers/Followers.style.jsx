346dd6f47e104a1e5bb58092f8bbd77ebc496e70
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-12-22 20:24:59 +0900

    Design: 팔로워 페이지 레이아웃 수정

--- src/Pages/Main/Profile/Followers/Followers.style.jsx
+++ src/Pages/Main/Profile/Followers/Followers.style.jsx
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
 export const FollowersWrapper = styled.section`
-  height: calc(100% - 108px);
-  max-width: 780px;
-  margin: 0 auto;
   padding: 24px 16px;
-  overflow-y: auto;
 `;
