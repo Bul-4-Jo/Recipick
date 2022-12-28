2c35a5320e70b15b208d92f5d28767e408635617
yunkooo <yunkoooooo@gmail.com>
2022-12-28 23:16:44 +0900

    Fix: alert창 portal을 이용해서 구현

--- src/Components/Common/Alert/Alert.jsx
+++ src/Components/Common/Alert/Alert.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { AlertWrapper, AlertBtnStyle, AlertTit } from './Alert.style';
+import ReactDOM from 'react-dom';
+import { AlertWrapper, AlertBtnStyle, AlertTit, BackgroundBlur } from './Alert.style';
 
-export default function Alert({ alertMSG, cancelFunc, rightMSG, rightFunc }) {
-  return (
-    <AlertWrapper>
-      <AlertTit>{alertMSG}</AlertTit>
-      <AlertBtnStyle onClick={cancelFunc}>취소</AlertBtnStyle>
-      <AlertBtnStyle className='actBtn' onClick={rightFunc}>
-        {rightMSG}
-      </AlertBtnStyle>
-    </AlertWrapper>
+export default function Alert({ alertMSG, rightMSG, rightFunc, stateFunc }) {
+  const modal = document.getElementById('modal-root');
+
+  const cancelFunc = () => {
+    stateFunc(prev => !prev);
+  };
+
+  const bgClickHandler = () => {
+    stateFunc(prev => !prev);
+  };
+
+  return ReactDOM.createPortal(
+    <BackgroundBlur onClick={bgClickHandler}>
+      <AlertWrapper>
+        <AlertTit>{alertMSG}</AlertTit>
+        <AlertBtnStyle onClick={cancelFunc}>취소</AlertBtnStyle>
+        <AlertBtnStyle className='actBtn' onClick={rightFunc}>
+          {rightMSG}
+        </AlertBtnStyle>
+      </AlertWrapper>
+    </BackgroundBlur>,
+    modal
   );
 }
