0013b106447f9f76699beec1100c92867738f888
강수정 <kngsujng@naver.com>
2022-12-29 17:45:28 +0900

    Feat: 설정 및 개인정보 모달 프로필 설정으로 이동

--- src/Components/Common/Header/Header.jsx
+++ src/Components/Common/Header/Header.jsx
@@ -54,7 +54,7 @@ export default function Header() {
   const listObj = [
     {
       name: '설정 및 개인정보',
-      func: () => console.log('설정 및 개인정보'),
+      func: () => navigate('/profile/:accountname/edit'),
     },
     {
       name: '로그아웃',
