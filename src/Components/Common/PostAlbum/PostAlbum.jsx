ea88945333723672f7d51d61afba0f8ee186b622
강수정 <kngsujng@naver.com>
2022-12-31 03:41:58 +0900

    Feat: 앨범형 사진 클릭시 PostDetail 페이지로 이동

--- src/Components/Common/PostAlbum/PostAlbum.jsx
+++ src/Components/Common/PostAlbum/PostAlbum.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { PostAlbumWrapper, AlbumImgItem } from './PostAlbum.style';
 import Iconlayers from './../../../Assets/Icons/icon_layers.png';
 
-export default function PostAlbum({ postImg }) {
+export default function PostAlbum({ postImg, postDetail }) {
   const [img, setImg] = useState([]);
+  const id = postDetail;
 
   useEffect(() => {
     if (!postImg) {
@@ -21,26 +23,30 @@ export default function PostAlbum({ postImg }) {
       <PostAlbumWrapper>
         {postImg &&
           (img.length > 1 ? (
-            <AlbumImgItem>
-              <img
-                className='multiImage'
-                src={`https://mandarin.api.weniv.co.kr/${img[0]}`}
-                alt='사용자가 업로드한 이미지'
-              />
-              <img
-                className='layerIcon'
-                src={Iconlayers}
-                alt='사용자가 업로드한 이미지가 여러 개인 것을 알려주는 아이콘'
-              />
-            </AlbumImgItem>
+            <Link to={`/post/${id}`}>
+              <AlbumImgItem>
+                <img
+                  className='multiImage'
+                  src={`https://mandarin.api.weniv.co.kr/${img[0]}`}
+                  alt='사용자가 업로드한 이미지'
+                />
+                <img
+                  className='layerIcon'
+                  src={Iconlayers}
+                  alt='사용자가 업로드한 이미지가 여러 개인 것을 알려주는 아이콘'
+                />
+              </AlbumImgItem>
+            </Link>
           ) : (
-            <AlbumImgItem>
-              <img
-                className='multiImage'
-                src={`https://mandarin.api.weniv.co.kr/${img[0]}`}
-                alt='사용자가 업로드한 이미지'
-              />
-            </AlbumImgItem>
+            <Link to={`/post/${id}`}>
+              <AlbumImgItem>
+                <img
+                  className='multiImage'
+                  src={`https://mandarin.api.weniv.co.kr/${img[0]}`}
+                  alt='사용자가 업로드한 이미지'
+                />
+              </AlbumImgItem>
+            </Link>
           ))}
       </PostAlbumWrapper>
     </>
