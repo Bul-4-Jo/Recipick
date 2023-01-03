ec58d3616ee6df15eee67a4fd1c991f2f1fe867b
hyesu.son <46313348+sduu@users.noreply.github.com>
2023-01-04 01:22:03 +0900

    Feat: 게시물 상세 페이지에 판매 중인 상품 추가 #159

--- src/Pages/Post/PostDetail/PostDetail.jsx
+++ src/Pages/Post/PostDetail/PostDetail.jsx
@@ -3,16 +3,27 @@ import { useParams } from 'react-router-dom';
 import Comment from '../../../Components/Comment/Comment';
 import PostCard from '../../../Components/Common/PostCard/PostCard';
 import { getPostDetail } from '../../../API/api';
+import Product from './../../../Components/Product/Product';
 
 export default function PostDetail() {
   const { postid } = useParams();
   const [isRender, setIsRender] = useState(false);
 
   const [postDetail, setPostDetail] = useState();
+  const [tagList, setTagList] = useState([]);
 
   useEffect(() => {
     getPostDetail(postid).then(response => {
       setPostDetail(prev => response);
+      try {
+        const contentObj = JSON.parse(response.post.content);
+
+        setTagList(contentObj.tagList || []);
+      } catch (error) {
+        if (error instanceof SyntaxError) {
+          setTagList([]);
+        }
+      }
     });
     setIsRender(true);
   }, []);
@@ -31,7 +42,9 @@ export default function PostDetail() {
           key={crypto.randomUUID()}
           commentCount={postDetail.post.commentCount}
           postid={postDetail.post.id}
-        />
+        >
+          <Product accountName={postDetail.post.author.accountname} tagList={tagList} />
+        </PostCard>
         <Comment />
       </>
     )
