5e32ec53a85b9796d57eca7ec90d6468d231d8ed
강수정 <kngsujng@naver.com>
2023-01-19 17:41:23 +0900

    Feat: 게시물 작성시 레시피 템플릿 제공

--- src/Components/PostEdit/PostText/PostText.jsx
+++ src/Components/PostEdit/PostText/PostText.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
-import { PostTextArea } from './PostText.style';
+import { PostTextArea, PostPreview, PostTextWrapper } from './PostText.style';
 
-export default function PostText({ textValue, setTextValue }) {
+export default function PostText1({ textValue, setTextValue }) {
   const textareaRef = useRef();
   const resizeTextArea = target => {
     target.style.height = '1px';
@@ -32,16 +32,37 @@ export default function PostText({ textValue, setTextValue }) {
 
   return (
     <>
-      <label className='screen-out' htmlFor='txtRecipe'>
-        레시피 입력
-      </label>
-      <PostTextArea
-        ref={textareaRef}
-        value={textValue}
-        onChange={onChangeHandler}
-        id='txtRecipe'
-        placeholder='레시피 입력하기...'
-      ></PostTextArea>
+      <PostTextWrapper>
+        <label className='screen-out' htmlFor='txtRecipe'>
+          레시피 입력
+        </label>
+        <PostPreview>
+          <label htmlFor='dishName'>1. 요리 이름 : </label>
+          <input type='text' id='dishName' />
+          <label htmlFor='cookingTime'>2. 예상 조리 시간 : </label>
+          <input type='number' id='cookingTime' />
+          <p>3. 난이도 : </p>
+          <div>
+            <label htmlFor='high'>
+              상 <input type='radio' id='high' name='difficulty' />
+            </label>
+            <label htmlFor='middle'>
+              중 <input type='radio' id='middle' name='difficulty' />
+            </label>
+            <label htmlFor='low'>
+              하 <input type='radio' id='low' name='difficulty' />
+            </label>
+          </div>
+          <label>4. 조리 순서 : </label>
+        </PostPreview>
+        <PostTextArea
+          ref={textareaRef}
+          value={textValue}
+          onChange={onChangeHandler}
+          id='txtRecipe'
+          placeholder='레시피 입력하기...'
+        ></PostTextArea>
+      </PostTextWrapper>
     </>
   );
 }
